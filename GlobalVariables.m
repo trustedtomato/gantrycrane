9f94bea1c49daf0c37123c16bbc276fee8a3f545
Tamas Halasi <trusted.tomato@gmail.com>
2023-04-01 17:54:49 +0200

    use GlobalVariables to initalize parameter tuning, add pendulum tuning to parameter tuning, and make parameter tuning file less repetitive

--- GlobalVariables.m
+++ GlobalVariables.m
@@ -1,18 +1,24 @@
-La = 1.02*10^(-6);
 Ra = 4.2;
 Jm = 0.0000218;
 rm = 0.7/100;
-b_rot = 62.892 * rm^2;
-Kt = 0.138;
-Ke = 0.1377;
+Dsm = 62.892;
+kt = 0.138;
+ke = 0.1377;
 ms = 0.93;
 mr = 0.085;
 ml = 0.184;
 hl = 1.5/100;
 Lr = 24.1/100;
-Lcm =  Lr + hl/2;
+Lcm = Lr + hl/2;
 g = 9.82;
 Dp = 0.007;
-bp = Dp * 10.5/100;%(mr*Lr/2+ml*Lcm)/(mr+ml); Center mass of pendulum weighted average
-rl = 2.08/100;
-Jp = 1/3*mr*Lr^2+1/2*ml*rl^2+ml*Lcm^2; %Parameter list says 0.016???
+Jp = 0.016; %Parameter list says 0.016???
+mj = ms * Jm;
+
+% Use the most up-to-date good gene we found for the sledge model
+newValues = num2cell([6.50966186168443e-05 2.54521253558318 0.418590929040413 0.0706376302151075]);
+[mj, Dsm, kt, ke] = newValues{:};
+
+% Use the most up-to-date good gene we found for the pendulum model
+newValues = num2cell([0.0155847761214082 0.00264359975409834]);
+[Jp, Dp] = newValues{:};
--- Paramater tuning/ParamaterTuningGACombinedMJ.m
+++ Paramater tuning/ParamaterTuningGACombinedMJ.m
@@ -1,38 +1,47 @@
 %% Initialize variables
 clc
 clear
+
+global model shouldTrainPendulum Lcm ml Lr mr g Jp Dp kt Ra rm mj ke Dsm;
+
 load('GraneResponsesWithVoltageOffset.mat');
-Ra = 4.2;
-Jm = 0.0000218;
-rm = 0.7/100;
-Dsm = 62.892;
-kt = 0.138;
-ke = 0.1377;
-ms = 0.93;
-mr = 0.085;
-ml = 0.184;
-hl = 1.5/100;
-Lr = 24.1/100;
-Lcm =  Lr + hl/2;
-g = 9.82;
-Dp = 0.007;
-Jp = 0.016; %Parameter list says 0.016???
-mj = ms * Jm;
+load('IIRAngleFilterObject.mat')
+run('../GlobalVariables.m')
+
+
 inputs = [small_bang_input, small_ramp_input, small_step_input, small_sine_input, big_bang_input, big_ramp_input, big_step_input, big_sine_input];
 sledgeResponses = [small_bang_sledge, small_ramp_sledge, small_step_sledge, small_sine_sledge, big_bang_sledge, big_ramp_sledge, big_step_sledge, big_sine_sledge];
+pendulumResponses = [small_bang_angle, small_ramp_angle, small_step_angle, small_sine_angle, big_bang_angle, big_ramp_angle, big_step_angle, big_sine_angle];
+sledgeWeights = [1 1 1 1 1 1 1 1];
+pendulumWeights = [1 0 1 1 1 0 1 1];
+
+for i = 1:length(pendulumResponses)
+    pendulumResponses(i).Data = filter(IIRAngleFilter, pendulumResponses(i).Data);
+end
+
+shouldTrainPendulum = true;
+
+initialGene = [mj Dsm kt ke];
+responses = sledgeResponses;
+weights = sledgeWeights;
+if shouldTrainPendulum
+    initialGene = [Jp Dp];
+    responses = pendulumResponses;
+    weights = pendulumWeights;
+end
 
 %% Make initial figure
 
-sledgeModel = tf([kt/(Ra*rm)], [mj/rm^2 kt*ke/(rm^2*Ra)+Dsm 0]);
+initalizeModel()
 
 figure(1)
 for i = 1:length(inputs)
     subplot(3,3,i);
-    y = lsim(sledgeModel, inputs(i).Data,inputs(i).Time);
+    y = lsim(model, inputs(i).Data,inputs(i).Time);
     hold off
     plot(inputs(i).Time, y, 'Color', 'blue');
     hold on
-    plot(sledgeResponses(i).Time, sledgeResponses(i).Data, 'Color', 'red')
+    plot(responses(i).Time, responses(i).Data, 'Color', 'red')
 end
 
 %% Evolutionize genes
@@ -43,15 +52,10 @@ maxGens = 50;
 numberOfElites = 3;
 numberOfMutations = popSize - numberOfElites;
 
-
-initialGene = [mj Dsm kt ke];
-
 pop=repmat(initialGene, [popSize 1]); %repmat(initialGene, [popSize 1]);
 
 sigmaVec = initialGene;
 
-
-weights = [1 1 1 1 1 1 1 1];
 for i = 1:length(inputs)
     weights(i) = 1/(sum(inputs(i).Data.^2) * length(inputs(i).Time));
 end
@@ -61,12 +65,10 @@ for generation = 1:maxGens
     pop = abs(pop);
     for i = 1:length(inputs)
         for j = 1:popSize
-            newValues = num2cell(pop(j,:));
-            [mj, Dsm, kt, ke] = newValues{:};
-            sledgeModel = tf([kt/(Ra*rm)], [mj/rm^2 kt*ke/(rm^2*Ra)+Dsm 0]);
-   
-            x = lsim(sledgeModel, inputs(i).Data, inputs(i).Time);
-            xHat = sledgeResponses(i).Data;
+            initalizeModel(pop(j,:))
+
+            x = lsim(model, inputs(i).Data, inputs(i).Time);
+            xHat = responses(i).Data;
             error = calculateError(x, xHat);
             popFitnesses(j) = popFitnesses(j) + error*weights(i);
         end
@@ -95,26 +97,36 @@ end
 
 [a, bestThisGeneration] = min(popFitnesses);
 bestGene = pop(bestThisGeneration, :);
-newValues = num2cell(bestGene);
-[mj, Dsm, kt, ke] = newValues{:};
-sledgeModel = tf([kt/(Ra*rm)], [mj/rm^2 kt*ke/(rm^2*Ra)+Dsm 0]);
+initalizeModel(bestGene)
 
 figure(2)
 for i = 1:length(inputs)
     subplot(3,3,i);
-    y = lsim(sledgeModel, inputs(i).Data,inputs(i).Time);
+    y = lsim(model, inputs(i).Data,inputs(i).Time);
     hold off
     plot(inputs(i).Time, y, 'Color', 'blue');
     hold on
-    plot(sledgeResponses(i).Time, sledgeResponses(i).Data, 'Color', 'red')
+    plot(responses(i).Time, responses(i).Data, 'Color', 'red')
 end
-
 
 function error = calculateError(x, xHat)
     error = sum((x-xHat).^2);
 end
 
+function initalizeModel(gene)
+    global model shouldTrainPendulum Lcm ml Lr mr g Jp Dp kt Ra rm mj ke Dsm;
 
-
-
-
+    if shouldTrainPendulum
+        if exist('gene', 'var')
+            newValues = num2cell(gene);
+            [Jp, Dp] = newValues{:};
+        end
+        model = tf([kt/(Ra*rm)], [mj/rm^2 kt*ke/(rm^2*Ra)+Dsm 0]) * tf([(Lcm * ml + Lr/2*mr) 0 0], [Jp Dp (Lcm * ml + Lr * mr / 2)*g]);
+    else
+        if exist('gene', 'var')
+            newValues = num2cell(gene);
+            [mj, Dsm, kt, ke] = newValues{:};
+        end
+        model = tf([kt/(Ra*rm)], [mj/rm^2 kt*ke/(rm^2*Ra)+Dsm 0]);
+    end
+end
