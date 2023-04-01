a381b96f6d2bb70e3296dd149d92bcb6c3d8d2fd
Tamas Halasi <trusted.tomato@gmail.com>
2023-04-01 15:47:49 +0200

    clean up files

---
+++ Paramater tuning/FFTGeneration.m
@@ -0,0 +1,21 @@
+
+load("GraneResponses.mat");
+load("IIRAngleFilterObject.mat")
+
+data = test_angle.Data; % Data
+t = test_angle.Time;        % Time vector
+T = t(2) - t(1);             % Sampling period       
+Fs = 1/T;            % Sampling frequency                    
+N = length(t);             % Length of signal
+
+
+fourierData = fft(data);
+P2 = abs(fourierData/N);
+P1 = P2(1:floor(N/2)+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+f = Fs*(0:(N/2))/N;
+plot(f,P1) 
+title("Single-Sided Amplitude Spectrum of X(t)")
+xlabel("f (Hz)")
+ylabel("|P1(f)|")
