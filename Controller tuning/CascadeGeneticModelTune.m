8b0342519dd7a8ea5393a5fd48320cd0b5b5997c
tuebjensen <tuebjensen@gmail.com>
2023-04-04 14:52:39 +0200

    make cascade control tuning work

--- Controller tuning/CascadeGeneticModelTune.m
+++ Controller tuning/CascadeGeneticModelTune.m
@@ -1,24 +1,25 @@
 clear
 clc
 
-run("../GlobalVariables.m");
 
-%initialGene = [6.02219427682515,0.719683327883970,0.00794559323549371,0.267179698905954];
-initialGene = [1 1 1 1];
+
+initialGene = [6.02219427682515,0.719683327883970,0.00794559323549371,0.267179698905954];
 newValues = num2cell(initialGene);
 [p_in, p_out, i_in, i_out] = newValues{:};
 
-optimizedGene = optimizeGene(initialGene, 10, 10, @fitnessFunction);
+optimizedGene = optimizeGene(initialGene, 3, 3, @fitnessFunction);
 
 function fitness = fitnessFunction (gene)
+    run("../GlobalVariables.m");
+    options = simset('SrcWorkspace', 'current');
     newValues = num2cell(gene);
     [p_in, p_out, i_in, i_out] = newValues{:};
     
     setPoint = 0.5;
-    
+
     % TODO: use setPoint as the set point, and rename p_in to pIn, etc.
     % sets positionResponse and angleResponse
-    sim("../controllers/CascadeNew", 17);
+    sim("../controllers/CascadeNew", 17, options);
 
     fitness = getFitness([positionResponse.Time positionResponse.Data], setPoint, [angleResponse.Time angleResponse.Data]);
 end
