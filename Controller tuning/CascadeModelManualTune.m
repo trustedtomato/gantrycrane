60e5be57d0cc26a9eaf459ec84b36b673a255fad
Norbert_Pap <pap.norbert98@gmail.com>
2023-04-24 21:52:06 +0200

    added outer loop to cascade manual tuning

--- Controller tuning/CascadeModelManualTune.m
+++ Controller tuning/CascadeModelManualTune.m
@@ -4,10 +4,9 @@ clc
 addpath('../')
 %%
 run("GlobalVariables.m");
-% load('../Results/NoisyAngleResponse.mat');
 options = simset('SrcWorkspace', 'current');
 
-
+figure(1)
 KpArray = [0, 1, 10, 25, 33, 45, 75, 100, 200];
 colorMap = turbo(length(KpArray));
 legendInfo = cell(1, length(KpArray));
@@ -25,6 +24,48 @@ for i = 1:length(KpArray)
     legendInfo{i} = ['Kp = ' num2str(Kp)];
 end
 
+% add legend to plot
+legend(legendInfo)
+% add labels to plot
+xlabel('Time [s]')
+ylabel('Angle [deg]')
+
+figure(2)
+subplot(2,1,1);
+hold on;
+grid on;
+subplot(2,1,2);
+hold on;
+grid on;
+KpArray = [0.1, 0.5, 1, 2, 5, 10, 20, 50, 75, 100];
+colorMap = turbo(length(KpArray));
+legendInfo = cell(1, length(KpArray));
+for i = 1:length(KpArray)
+    Kp = KpArray(i);
+    Ki = 0;
+    
+    % sets positionResponse and angleResponse
+    sim("controllers\CascadeModelBothLoopsForTuning.slx", 17, options);
+    subplot(2,1,1);
+    hold on
+    % plot(positionResponse.time, positionResponse.data, 'Color', colorMap(i, :))
+    plot(positionResponse.time, positionResponse.data)
+    subplot(2,1,2);
+    hold on
+    % plot(angleResponse.time, angleResponse.data, 'Color', colorMap(i, :))
+    plot(angleResponse.time, angleResponse.data)
+    % add legend containing the Kp values and the fitness values through getFitness
+    legendInfo{i} = ['Kp = ' num2str(Kp) ' Fitness = ' num2str(getFitness([positionResponse.time positionResponse.data], 0.5, [angleResponse.time angleResponse.data]))];
+    
+end
+
+subplot(2,1,1);
+% add legend to plot
+legend(legendInfo)
+% add labels to plot
+xlabel('Time [s]')
+ylabel('Position [m]')
+subplot(2,1,2);
 % add legend to plot
 legend(legendInfo)
 % add labels to plot
